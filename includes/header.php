f7339719c31c076b9110b6defdb5d2499652b719
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-01-27 01:11:52 -0800

    minor file change to swapping out credentials

--- includes/header.php
+++ includes/header.php
@@ -5,9 +5,8 @@
         <meta name="viewport" content="width=device-width" />
         <meta name="robots" content="noindex,nofollow" />
         <meta charset="utf-8">
-        <script src="https://use.fontawesome.com/6a71565c22.js"></script>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
         <script src='https://www.google.com/recaptcha/api.js?render=6LeUT4kUAAAAAF_Mw3YbkVb6GZKRYugT2SUnJj58'></script>
+        <script src="https://use.fontawesome.com/6a71565c22.js"></script>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
         <link rel="stylesheet" href= "<?=CSS_PATH; ?>nav.css" />
         <link rel="stylesheet" href="<?=CSS_PATH; ?>portal.css" />
