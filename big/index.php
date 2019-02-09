f788f62b85ad698465f9f428f36e7a91510d9879
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-02-08 21:08:27 -0800

    initial big upload

--- big/index.php
+++ big/index.php
@@ -1,11 +1,27 @@
-<?php include '../includes/config.php'?>
-<?php include '../includes/header.php'?>
-
-
+<?php include 'includes/config.php'?>
+<?php include 'includes/header.php'?>    
+<!-- START LEFT COL -->
         <section>
-            <h2 class="pageID">BIG project - Soon to come</h2>
-            <p>This will be cool, but it comes later in the quarter</p>
- 
+            <h2 class="pageID"><?=$sectionHeading?></h2>
+            <?php
+            /*
+             * Below are 2 different forms to be re-used       
+             * 
+             * Only use one at a time, comment out the other 1!       
+             *
+             */
+            include 'includes/multiple.php'; #multiple element contact form
+            ?>
+            <p class="clear-recaptcha"></p>
         </section>
+<!-- END LEFT COL -->
 
-<?php include '../includes/footer.php'?>
+<!-- START RIGHT COL -->
+        <aside>
+            <BR /><BR />
+            <p>list of client resources </p>
+            <p>list some of my favorite sites that helps clients - color-responsive-etc</p>
+        </aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php'?>
