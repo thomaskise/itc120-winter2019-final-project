5356765fc64ffc3fa4edcd8fa60978af829119e9
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-02-08 21:12:09 -0800

    Delete extraneous file

--- big/google.php
+++
@@ -1,8 +0,0 @@
-<?php include 'includes/config.php'?>
-<?php include 'includes/header.php'?>   
-
-<section>
-    <h2 class="pageID"><?=$sectionHeading;?></h2>
-</section>
-
-<?php include 'includes/footer.php'?>
