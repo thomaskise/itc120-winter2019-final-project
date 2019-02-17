f78c5766901d6d087a13c3cc979f54da6e6e782b
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-02-17 12:03:43 -0800

    add flowchart & try embeded options

---
+++ flowchart.php
@@ -0,0 +1,20 @@
+<?php include 'includes/config.php';?>
+<?php include 'includes/header.php';?>
+<!-- START LEFT COL -->
+        <section>
+            <h2 class="pageID"><?=$pageHeading?></h2>
+
+<!--
+            <iframe src="https://docs.google.com/gview?url=https://docs.google.com/document/d/1HvQQBEsQHhYvLO14KQL9gMVafE9q6pyH1XV6Xd0sE2E/edit?usp=sharing&embedded=true" style="width=100%; height:100%;" frameborder="0">
+            </iframe>
+            
+-->
+            
+<!--            <div class="pageWrapper">-->
+            <div>
+                <iframe id="inlineframe" class="pageWrapper" width="100%" height="100%" src="images/bigresearch2.pdf" style="width=800px" "height:800px" allowfullscreen></iframe>
+            </div>
+
+
+       
+<?php include 'includes/footer.php';?>
