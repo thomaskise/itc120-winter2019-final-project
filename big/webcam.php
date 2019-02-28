b71e93de1f1f3fd224fc766569aaa67f11868963
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-02-28 01:36:19 -0800

    latest try at webcam

--- big/webcam.php
+++ big/webcam.php
@@ -4,9 +4,16 @@
 <section>
     <h2 class="pageID"><i class="<?=$logo?>"></i><?=$sectionHeading;?></h2>
     <div class='embed-container'>
-        <iframe a href="(https://www.wsdot.com/traffic/seattle/default.aspx?cam=1027#cam)" target="_blank"><img src="(https://images.wsdot.wa.gov/nw/090vc00245.jpg?t=636866533218385002)" alt="WA DOT Live Web Cam" border="0" title="WA DOT Live Web Cam" /></a>
-    </iframe>
+        <iframe>
+            <iframe src="https://images.wsdot.wa.gov/nw/090vc00245.jpg"></iframe>
+        </iframe>
     </div>
 </section>
 
+<!--
+<td><a href="camera1_l.jpg"><img src="camera1_s_16965.gif" border="1" alt="" hspace="10" width="128" height="96"></a></td>
+<td align="center">
+<a href="camera1_l.jpg">Red Square Cam</a> (Feb 28 01:08, 111KB)</A>-->
+<!--"https://images.wsdot.wa.gov/nw/090vc00245.jpg?t=636866533218385002" -->
+<!--            <a href="https://www.wsdot.com/traffic/seattle/default.aspx" target="_blank"><img src="https://images.wsdot.wa.gov/nw/090vc00245.jpg" alt="WA DOT Live Web Cam" border="0" title="WA DOT Live Web Cam" /></a>-->
 <?php include 'includes/footer.php'?>
