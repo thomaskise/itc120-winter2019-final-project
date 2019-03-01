4e320721106c53e56cb6d285c753d825f722afd1
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-02-28 22:50:26 -0800

    update webcam and youtube

--- big/webcam.php
+++ big/webcam.php
@@ -3,17 +3,14 @@
 
 <section>
     <h2 class="pageID"><i class="<?=$logo?>"></i><?=$sectionHeading;?></h2>
-    <div class='embed-container'>
-        <iframe>
-            <iframe src="https://images.wsdot.wa.gov/nw/090vc00245.jpg"></iframe>
-        </iframe>
+    <div class='embed-webcam'>
+        <p>WSDOT Mercer St</p>
+        <img id="ILPTrafficCamera" src="https://images.wsdot.wa.gov/nw/005vc16702.jpg?t=636869788305036637" alt="Mercer&#32;St" style="height:249px;width:335px;" />
+    </div>
+    <div class='embed-webcam'>
+        <p>UW Red Square</p>
+        <img id="uwcam" src="https://www.washington.edu/cambots/camera1_s_8707.gif" alt="redsq" style="height:249px;width:335px;" />
     </div>
 </section>
 
-<!--
-<td><a href="camera1_l.jpg"><img src="camera1_s_16965.gif" border="1" alt="" hspace="10" width="128" height="96"></a></td>
-<td align="center">
-<a href="camera1_l.jpg">Red Square Cam</a> (Feb 28 01:08, 111KB)</A>-->
-<!--"https://images.wsdot.wa.gov/nw/090vc00245.jpg?t=636866533218385002" -->
-<!--            <a href="https://www.wsdot.com/traffic/seattle/default.aspx" target="_blank"><img src="https://images.wsdot.wa.gov/nw/090vc00245.jpg" alt="WA DOT Live Web Cam" border="0" title="WA DOT Live Web Cam" /></a>-->
 <?php include 'includes/footer.php'?>
--- big/youtube.php
+++ big/youtube.php
@@ -4,20 +4,17 @@
 <section>
     <h2 class="pageID"><i class="<?=$logo?>"></i><?=$sectionHeading;?></h2>
     <div class='embed-container'>
-        <iframe src='https://www.youtube.com/embed/Vl89g2SwMh4'>
+        <iframe src='https://www.youtube.com/embed/Vl89g2SwMh4' allowfullscreen>
         </iframe>
     </div>
 </section>
 <aside>
+    <br /><br />
     <h2 class="pageID">Rock-it!!</h2>
     <p>
         Jefferson Airplane<br />
         Woodstock (1967)
     </p>
-    <br /><br /><br />
-    <h4>
-        <a href="youtubefs.php" target="_blank">Let's see it full screen!</a>
-    </h4>
 </aside>
 
 <?php include 'includes/footer.php'?>
