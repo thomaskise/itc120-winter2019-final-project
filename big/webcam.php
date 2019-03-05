4ff4ee54c45abe006b5076d267cdd5326ea0e6f0
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-03-04 20:59:24 -0800

    get webcams in iframes

--- big/webcam.php
+++ big/webcam.php
@@ -1,16 +1,20 @@
 <?php include 'includes/config.php'?>
 <?php include 'includes/header.php'?>   
 
-<section>
+<section>    
     <h2 class="pageID"><i class="<?=$logo?>"></i><?=$sectionHeading;?></h2>
-    <div class='embed-webcam'>
-        <p>WSDOT Mercer St</p>
-        <img id="ILPTrafficCamera" src="https://images.wsdot.wa.gov/nw/005vc16702.jpg?t=636869788305036637" alt="Mercer&#32;St" style="height:249px;width:335px;" />
-    </div>
-    <div class='embed-webcam'>
+    <div class='embed-webcam-uw'>
         <p>UW Red Square</p>
-        <img id="uwcam" src="https://www.washington.edu/cambots/camera1_s_8707.gif" alt="redsq" style="height:249px;width:335px;" />
+        <iframe src="https://www.washington.edu/cambots/camera1_l.jpg" title="UW Red Square Webcam" style="height:480px;width:704px;"></iframe>
     </div>
 </section>
+<aside>
+    <br /><br /><br />
+    <div class='embed-webcam-dot'>
+        <p>WSDOT Mercer St</p>
+        <iframe src="https://images.wsdot.wa.gov/nw/005vc16702.jpg?t=636869788305036637" title="DOT Mercer St Webcam" style="height:274px;width:379px;"></iframe>
+<!--                style="height:249px;width:335px;">-->
+    </div>
+</aside>
 
 <?php include 'includes/footer.php'?>
